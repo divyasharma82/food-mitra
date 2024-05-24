9fdd733bd51012aeeabcfdf3186b454338ded06e
Divya Sharma <122516735+divyasharma82@users.noreply.github.com>
2024-05-24 15:53:45 +0530

    database setup done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,11 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import mongoose from 'mongoose';
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log('Connected to database successfully!'));
 
 const app = express();
 app.use(express.json());
